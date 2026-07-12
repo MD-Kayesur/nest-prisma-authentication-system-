6bacd03593b9dfe66270444f766ee2b2a2984b8b
MD-Kayesur <rmdkayesur@gmail.com>
2026-07-12 13:16:14 +0600

    implement base auth module with controller, service, and unit tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service'; // <-- 1. Import it here
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [
     AppService, 
@@ -13,3 +14,4 @@ import { PrismaModule } from './prisma/prisma.module';
   ],
 })
 export class AppModule {}
+ 
