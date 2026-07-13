ed85710b8582a3032b619d81f6a89688aa37d158
MD-Kayesur <rmdkayesur@gmail.com>
2026-07-13 15:14:20 +0600

    add register and login endpoints to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    @Post('register')
+    register() {
+        return 'user created successfully';
+    }
+
+    @Post('login')
+    login() {
+        return 'user login successfully';
+    }
+
+
+    
+}
