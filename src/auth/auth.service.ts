d60c6be85a08f6f9030b19e8a2c1a449e9ddd5e8
MD-Kayesur <rmdkayesur@gmail.com>
2026-07-14 11:15:22 +0600

    implement User module and integrate with AuthService and AppModule

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
+    constructor(private readonly userService: UserService){}
 register() {
 
 //logic for user register
---
+++ src/user/user.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+
+    createUser(){
+ return {message: "user created successfully"}        
+    }
+}
